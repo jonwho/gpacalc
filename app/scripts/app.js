8508e5e1ffbc805035dc158d3c8e1d39e9b82737
Jon Ho <housuilun@yahoo.com>
2014-09-05 03:23:36 -0700

    more templating

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,8 +9,26 @@
  * Main module of the application.
  */
 angular
-  .module('gpacalcApp', [
-    'ngCookies',
-    'ngTouch',
-    'ui.router'
-  ]);
+	.module('gpacalcApp', [
+		'ngCookies',
+		'ngTouch',
+		'ui.router'
+	])
+	.config(function($stateProvider, $urlRouterProvider) {
+
+		$urlRouterProvider.otherwise('/calculator');
+
+		$stateProvider
+			.state('calculator', {
+				url: '/calculator',
+				templateUrl: 'views/calculator.html'
+			})
+			.state('about', {
+				url: '/about',
+				templateUrl: 'views/about.html'
+			})
+			.state('contact', {
+				url: '/contact',
+				templateUrl: 'views/contact.html'
+			});
+	});
